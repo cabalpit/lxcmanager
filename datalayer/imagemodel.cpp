9beef5c7d52f4ea46675af39874f9c9870cf004b
Peter Cata <repire22b@hotmail.com>
2021-09-19 01:22:45 +0200

    Add comment and trigger setter

--- datalayer/imagemodel.cpp
+++ datalayer/imagemodel.cpp
@@ -2,6 +2,13 @@
 
 using namespace model;
 
+/**
+ * @brief ImageModel::ImageModel
+ *
+ * Default constructor, construct object.
+ *
+ * @param parent waits parent QObject default nullptr.
+ */
 ImageModel::ImageModel(QObject *parent) : ModelBase(parent)
 {
 	setTable("images");
